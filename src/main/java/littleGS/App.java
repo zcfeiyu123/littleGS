cfbb5202b612fc8e1cf5bb3f65f1005bdea983ed
zcfeiyu123 <zcfeiyu123@gmail.com>
2013-10-30 23:07:31 +0800

    modified by zhangcen

--- src/main/java/littleGS/App.java
+++ src/main/java/littleGS/App.java
@@ -1,5 +1,9 @@
 package littleGS;
 
+import io.netty.bootstrap.Bootstrap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        Bootstrap bootstrap = new Bootstrap();
+        Logger logger = LogManager.getLogger();
+        logger.info("some thing");
+        logger.fatal("eng??");
         System.out.println( "Hello World!" );
     }
 }
